734394dedb150aef5caf36304a1cc831bef41070
Mohamed Hossam <131800718+MeDoRa0@users.noreply.github.com>
2024-12-22 21:24:11 +0200

    splash view UI done

---
+++ lib/core/utils/assets.dart
@@ -0,0 +1,21 @@
+// ignore_for_file: prefer_single_quotes
+class Assets {
+  Assets._();
+  
+  /// Assets for imagesFruitLogo
+  /// assets/images/fruit_logo.png
+  static const String imagesFruitLogo = "assets/images/fruit_logo.png";
+
+  /// Assets for imagesLeafTop
+  /// assets/images/leaf_top.svg
+  static const String imagesLeafTop = "assets/images/leaf_top.svg";
+
+  /// Assets for imagesSplashBottom
+  /// assets/images/splash_bottom.svg
+  static const String imagesSplashBottom = "assets/images/splash_bottom.svg";
+
+  /// Assets for imagesSplashLogo
+  /// assets/images/splash_logo.svg
+  static const String imagesSplashLogo = "assets/images/splash_logo.svg";
+}
+
