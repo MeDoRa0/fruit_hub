25f698a095a8dc039912bd04028e7491e26e5681
Mohamed Hossam <131800718+MeDoRa0@users.noreply.github.com>
2025-01-14 22:33:28 +0200

    Add dartz dependency and implement authentication domain entities and repository

---
+++ lib/core/errors/failuers.dart
@@ -0,0 +1,7 @@
+class Failuers {
+ final String message;
+  Failuers({required this.message});
+}
+class ServerFailuer extends Failuers {
+  ServerFailuer({required super.message});
+}
---
+++ lib/features/authentication/domain/repository/auth_repo.dart
@@ -0,0 +1,11 @@
+import 'package:dartz/dartz.dart';
+import 'package:fruit_hub/core/errors/failuers.dart';
+import 'package:fruit_hub/features/authentication/domain/entites/user_entity.dart';
+
+abstract class AuthRepo {
+  Future<Either<Failuers, UserEntity>> createUserWithEmailAndPassword({
+    required String name,
+    required String email,
+    required String password,
+  });
+}
