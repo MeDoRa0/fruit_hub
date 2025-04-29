98a11971d011dd5368fd032f788fd05033bc6baf
Mohamed Hossam <131800718+MeDoRa0@users.noreply.github.com>
2025-04-29 22:39:13 +0300

    chore: Remove sensitive PayPal API credentials from app_keys.dart

---
+++ lib/core/utils/app_keys.dart
@@ -0,0 +1,4 @@
+class AppKeys {
+  static const String kPayPalClientId = 'AUGK7IJOGEAw64_VSs3G2geIC3y84NCHH_5rOba5lVMY2FRR93aE9No1nZKNVP8-9LASgTUDffP5SJli';
+  static const String kPayPalSecretKey = 'EIcSdBdAD6pqKJnReKYC6bJ4PX8x0g9NiJjhGO1m7FoX_NdtfEgAIUDYTjTxOLZZZe_fLGmbugleql0S';
+}
